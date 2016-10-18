e2641aca135a35ab90233c0f0feb3b6684686364
dsantacruz <diegomiguel114@gmail.com>
2016-10-17 20:00:27 -0500

    agree comment containers additem + itemlist

--- redux/src/containers/AddItem.js
+++ redux/src/containers/AddItem.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { addItem } from '../actions'
 
+//permite conectar el componente con la aplicacion para que se pueda visualizar, y permite llamar a dos funciones que podemos definir , para pasarle el estado, y pasarle las propiedades a los componentes.
 let AddItem = ({dispatch}) => {console.log("containers addItem.js");
     let input;
 
@@ -11,6 +12,7 @@ let AddItem = ({dispatch}) => {console.log("containers addItem.js");
                 onSubmit={e => {
                     e.preventDefault();
                     if(!input.value.trim()) return;
+                    //dispatch permite ejecutar acciones
                     dispatch(addItem(input.value));
                     input.value = '';
                 }}
--- redux/src/containers/ItemsList.js
+++ redux/src/containers/ItemsList.js
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
 import ItemList from '../components/ItemList'
 
+//podemos pasarle el estado a nuestro componentes
 const mapStateToProps = (state) => {
 	console.log("mapstatetoprops");
 	console.log(state);
@@ -9,6 +10,9 @@ const mapStateToProps = (state) => {
     }
 };
 
+//podemos llamar a cualquier accion de la aplicacion
+//podemos pasar acciones a nuestros componentes
+//le estoy dando logica al evento omItemClick
 const mapDispatchToProps = (dispatch) => {
     return {
         onItemClick: (id) => {
@@ -17,6 +21,8 @@ const mapDispatchToProps = (dispatch) => {
     }
 };
 
+//le paso el mapstatetoprops para pasarle el estado al componente itemlist
+//le paso el maptispatchtoprops para q ejecute la accion onItemClick
 const ItemsList = connect(mapStateToProps, mapDispatchToProps)(ItemList);
 
 export default ItemsList;
